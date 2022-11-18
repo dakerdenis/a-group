34764baba4976bde4e1ca18a71c0cc8de295749d
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-11-18 15:38:51 +0400

    added

--- index.php
+++ index.php
@@ -8,19 +8,25 @@
 
              <!---slider__block-->
              <div class="main__slider__block">
-                    Блок слайдера
+                 Блок слайдера
              </div>
              <!---about__block---->
              <div class="main__ablout__block">
-
+                 A-GROUP
+                 NƏ ÜÇÜN SEÇİLİR
+                 <br>
+                 раздел о нас на сайте,почему нас выбирают
              </div>
              <!---Mobile__app__block---->
              <div class="main__mobile__block">
-
+                <p>
+                Раздел с мобильным приложением
+                </p>
+                <img src="./styles/imgs/1.png" alt="">
              </div>
              <!---our__clients__block---->
              <div class="main__clients__block">
-
+                
              </div>
 
              <!---footer---->
